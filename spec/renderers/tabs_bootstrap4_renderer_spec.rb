9a18f9d98c3dc103e241581bae7d8a19705d031f
Renato Toth <renatomolinat@gmail.com>
2021-07-06 13:00:47 -0700

    Adding support to nav markup for bootstrap 4 tabs

--- spec/renderers/tabs_bootstrap4_renderer_spec.rb
+++ spec/renderers/tabs_bootstrap4_renderer_spec.rb
@@ -7,15 +7,36 @@ describe RailsBootstrapTabs::Renderers::TabsBootstrap4Renderer do
   let(:renderer) { RailsBootstrapTabs::Renderers::TabsBootstrap4Renderer.new(template) }
 
   describe '#render_tabs_wrapper' do
-    it 'renders wrapper' do
-      expect(renderer.render_tabs_wrapper { '[tabs_content]' }).to eq '<ul class="nav nav-tabs">[tabs_content]</ul>'
+    context 'nav_markup option is NOT nil' do
+      let(:renderer) { RailsBootstrapTabs::Renderers::TabsBootstrap4Renderer.new(template, { nav_markup: true } ) }
+
+      it 'renders nav wrapper' do
+        expect(renderer.render_tabs_wrapper { '[tabs_content]' }).to eq '<nav class="nav nav-tabs">[tabs_content]</nav>'
+      end
+    end
+
+    context 'nav_markup is nil' do
+      it 'renders ul wrapper' do
+        expect(renderer.render_tabs_wrapper { '[tabs_content]' }).to eq '<ul class="nav nav-tabs">[tabs_content]</ul>'
+      end
     end
   end
 
   describe '#render_tab' do
-    it 'renders tab' do
-      tab = double(options: { anchor: 'anch', link_class: 'lnk', active: true })
-      expect(renderer.render_tab(tab) { '[title]' }).to eq '<li class="nav-item"><a data-toggle="tab" class="nav-link active lnk" href="#anch">[title]</a></li>'
+    context 'nav_markup is NOT nil' do
+      let(:renderer) { RailsBootstrapTabs::Renderers::TabsBootstrap4Renderer.new(template, { nav_markup: true } ) }
+
+      it 'renders nav wrapper' do
+        tab = double(options: { anchor: 'anch', link_class: 'lnk', active: true })
+        expect(renderer.render_tab(tab) { '[title]' }).to eq '<a data-toggle="tab" class="nav-link active lnk" href="#anch">[title]</a>'
+      end
+    end
+
+    context 'nav_markup is nil' do
+      it 'renders tab' do
+        tab = double(options: { anchor: 'anch', link_class: 'lnk', active: true })
+        expect(renderer.render_tab(tab) { '[title]' }).to eq '<li class="nav-item"><a data-toggle="tab" class="nav-link active lnk" href="#anch">[title]</a></li>'
+      end
     end
   end
 
